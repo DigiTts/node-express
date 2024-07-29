c78309faa85c2efcd576cea7d13aebbc67c92410
David Mendoza <dvdgrg2@gmail.com>
2024-07-28 22:35:23 -0400

    Added Promotions and Partners Routes

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,29 +1,46 @@
 const express = require('express');
-const CampsiteRouter = express.Router();
+const campsiteRouter = express.Router();
 
-CampsiteRouter.route('/')
-// The code above is the equivalent of the following code in server.js:
+campsiteRouter.route('/')
 .all((req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     next();
 })
-
 .get((req, res) => {
     res.end('Will send all the campsites to you');
 })
-
 .post((req, res) => {
     res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
 })
-
 .put((req, res) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /campsites');
 })
-
 .delete((req, res) => {
     res.end('Deleting all campsites');
 });
 
-module.exports = CampsiteRouter;
+campsiteRouter.route('/:campsiteId')
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+})
+.put((req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
+
+module.exports = campsiteRouter;
